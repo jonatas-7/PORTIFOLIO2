1c36b5b0435d4327f13b739cb86d63e16d74a4eb
jonatas-7 <jonatascavolicardoso@gmail.com>
2025-02-17 20:17:36 -0300

    17/02

---
+++ index.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PORTIFOLIO2</title>
+
+
+    <style>
+
+    .root {
+        font-size: 12px;
+
+    }
+    * {
+        margin: 0;
+        padding: 0;
+        /  font-family: arial, helvetica, serif; */ 
+        
+    }
+
+    body {
+       /* background-image: url('imagens/logo.png');
+        background-repeat: no-repeat;
+        background-position: center;
+        background-size: 100%;
+        background-attachment: fixed; */
+
+        border: 10px solid blue;
+        width: 70;
+        height: 100;
+        overflow: hidden;
+    }
+
+    p {
+        /* border: 1px solid red; */
+        border-width: 10px;
+        border-style: solid;
+        border-color: blue;
+        font-size: 1rem;
+
+    }
+
+    h1 {
+        background: green;
+        display: inline-block;
+    
+    }
+
+    h1:hover {
+        background-color: red;
+    }
+
+    .logo {
+
+    }
+
+
+    </style>
+
+
+</head>
+<body>
+    <div class="interface">
+
+        <header class="cabecalho">
+            <nav class="menu">
+                <div class="logo">
+                    <img src="imagens/logo.png" alt='LOGO' width='150'>
+                </div>
+                <ul>
+                    <li>Home</li>
+                    <li>Serviços</li>
+                    <li>Produtos</li>
+                    <li>Portfólio</li>
+                    <li>Sobre</li>
+                    <li>Contato</li>
+                </ul>
+            </nav>
+
+
+            <div class="titles">
+                <h1 class="title">Entregando Serviços de Qualidade</h1>
+                <h1 class="subtitle">Soluções em T.I</h1>
+                <p class='paragraph'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur natus obcaecati, illo doloribus iusto sit numquam nam laboriosam?</p>
+            </div>
+
+            <div class="socialIcons">
+                <a href="#" target="_blank">Twitter</a>
+                <a href="#">Facebook</a>
+                <a href="#">LinkedIn</a>
+                <a href="#">Instagram</a>
+            </div>
+        </header>
+
+    </div>
+</body>
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+</html>
