2a91c5790d19075bf156fc9491854169468ea352
jonatas-7 <jonatascavolicardoso@gmail.com>
2025-02-24 20:23:37 -0300

    aula 24/02

--- index.php
+++ index.php
@@ -1,127 +1,57 @@
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html lang="pt-br">
 <head>
-
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>PORTIFOLIO2</title>
-
-
-    <style>
-
-    .root {
-        font-size: 12px;
-
-    }
-    * {
-        margin: 0;
-        padding: 0;
-        /  font-family: arial, helvetica, serif; */ 
-        
-    }
-
-    body {
-       /* background-image: url('imagens/logo.png');
-        background-repeat: no-repeat;
-        background-position: center;
-        background-size: 100%;
-        background-attachment: fixed; */
-
-        border: 10px solid blue;
-        width: 70;
-        height: 100;
-        overflow: hidden;
-    }
-
-    p {
-        /* border: 1px solid red; */
-        border-width: 10px;
-        border-style: solid;
-        border-color: blue;
-        font-size: 1rem;
-
-    }
-
-    h1 {
-        background: green;
-        display: inline-block;
-    
-    }
-
-    h1:hover {
-        background-color: red;
-    }
-
-    .logo {
-
-    }
-
-
-    </style>
-
-
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
-    <div class="interface">
 
-        <header class="cabecalho">
-            <nav class="menu">
-                <div class="logo">
-                    <img src="imagens/logo.png" alt='LOGO' width='150'>
-                </div>
-                <ul>
-                    <li>Home</li>
-                    <li>Serviços</li>
-                    <li>Produtos</li>
-                    <li>Portfólio</li>
-                    <li>Sobre</li>
-                    <li>Contato</li>
-                </ul>
-            </nav>
+<div id="interface">
 
+    <header class="cabecalho">
 
-            <div class="titles">
-                <h1 class="title">Entregando Serviços de Qualidade</h1>
-                <h1 class="subtitle">Soluções em T.I</h1>
-                <p class='paragraph'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur natus obcaecati, illo doloribus iusto sit numquam nam laboriosam?</p>
+      <div class="container-header">
+
+        <nav class="menu">
+
+            <div class="logo">
+                <img src="imagens/logo.png" alt="LOGO" width="150" />
             </div>
 
-            <div class="socialIcons">
-                <a href="#" target="_blank">Twitter</a>
-                <a href="#">Facebook</a>
-                <a href="#">LinkedIn</a>
-                <a href="#">Instagram</a>
-            </div>
-        </header>
+            <ul>
+                <li>Home</li>
+                <li>Serviços</li>
+                <li>Produtos</li>
+                <li>Portifólio</li>
+                <li>Sobre</li>
+                <li>Contato</li>
+            </ul>
 
-    </div>
+        </nav>
+
+        <div class="titles">
+
+            <h1 class="titulo">Entregando serviços de qualidade</h1>
+            <h1 class="subtitulo">Soluções em TI</h1>
+
+            <p class="paragrafo">Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam quibusdam ab in! A, nam voluptas!</p>
+
+        </div>
+
+        <div class="socialIcons">
+            <a href="#">Twitter</a>
+            <a href="#">Facebook</a>
+            <a href="#">LinkedIn</a>
+            <a href="#">Instagram</a>
+        </div>
+
+      </div>
+
+    </header>
+
+</div>
+    
 </body>
-
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
 </html>
